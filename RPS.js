84af9c10cf9093197482b4168a4469f5a76c0197
Ethan McMahan <cowboyfanet@hotmail.com>
2022-10-24 18:48:57 -0700

    able to play one round.

--- RPS.js
+++ RPS.js
@@ -1,7 +1,6 @@
 
 function getComputerChoice() {
     let randomNumber = (Math.floor(Math.random() * 3))
-	console.log("randomNumber", randomNumber);
   if (randomNumber === 0){
     return "Rock";
   }
@@ -13,23 +12,20 @@ function getComputerChoice() {
   }
   }
 
-function playRound(playerSelection, computerSelection, score) {
+function playRound(playerSelection, computerSelection) {
 	if (((playerSelection == 'Rock') && (computerSelection == 'Scissors'))  ||
 	   ((playerSelection == 'Paper') && (computerSelection == 'Rock'))      ||
 	   ((playerSelection == 'Scissors') && (computerSelection == 'Paper'))) {
 		console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-		score[0]++;
 	} else if (playerSelection == computerSelection) {
 		console.log("Tie!");
 	} else {
 		console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-		score[1]++;
 
 	}
-	return score;
 }
-
 let userChoice = (prompt ("Enter Rock, Paper, or Scissors"));
 let computerChoice = getComputerChoice();
 console.log("user", userChoice);
 console.log("computer", computerChoice);
+playRound(userChoice, computerChoice);
