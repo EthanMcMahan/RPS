78b1902efc31107a35c955ff6f59f01f4b6a2d9e
Ethan McMahan <cowboyfanet@hotmail.com>
2022-10-26 19:06:16 -0700

    figured out how to play 5 rounds of game.

--- RPS.js
+++ RPS.js
@@ -5,19 +5,16 @@ and then we'll multiply by 3 to get a random number between (0 and 3), which he'
 the computers choice of "rock, paper, or scissors"*/
 //use if and else if conditional statements to step up the computer's choice scenerio
 //this function and conditional will allow us to enable the computer to randomly select their choice of play
+let choices = ["Rock", "Paper", "Scissors"];
+choices[0];
+choices[1];
+choices[2];
 
 function getComputerChoice() {
     let randomNumber = (Math.floor(Math.random() * 3))
-  if (randomNumber === 0){
-    return "Rock";
+    return choices[randomNumber];
   }
-  else if (randomNumber === 1){
-    return "Paper";
-  }
-  else if (randomNumber === 2){
-  return "Scissors";
-  }
-  }
+//refactored with an array inside of if/ else if statements
 
 //create function playRound with parameters (playerSelection and computeSelection)
 //use if else if and else conditionals to play out all the possible scenerios for one round of game
@@ -29,23 +26,22 @@ with the || operator to continue our string of logic*/
 //console.log our variables userChoice and computerChoice
 //call our function playRound using our variables userChoice and computerChoice
 
+let userScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
 	if (((playerSelection == 'Rock') && (computerSelection == 'Scissors'))  ||
 	   ((playerSelection == 'Paper') && (computerSelection == 'Rock'))      ||
 	   ((playerSelection == 'Scissors') && (computerSelection == 'Paper'))) {
 		console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    userScore++;
 	} else if (playerSelection == computerSelection) {
 		console.log("Tie!");
 	} else {
 		console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-
+    computerScore++;
 	}
 }
-let userChoice = (prompt ("Enter Rock, Paper, or Scissors"));
-let computerChoice = getComputerChoice();
-console.log("user", userChoice);
-console.log("computer", computerChoice);
-playRound(userChoice, computerChoice);
+
 
 //create a function (game)-after loop works put in function and call it
 //use a for loop to play 5 rounds-start with for loop that counts from 0 to 5
@@ -53,15 +49,42 @@ playRound(userChoice, computerChoice);
 //show who won each round
 //score user: 1, computer:0
 //count is only new variable, which is score
+//how do I make game selection different every round-different result?
+//how do I get score variable to increment by 1? (score++)
 
-let score = 0;
-let userScore = 0;
-let compScore = 0;
+/*function game() {
+  for (let round = 0; round < 5; round ++) {
+    console.log(round);
+    playRound(userChoice, computerChoice);
+  }
+}
+game();*/
+
+/*function game(playRound) {
+  for (let round = 0; round < 5; round++) {
+  console.log(round);
+  let score = playRound(userChoice, computerChoice);
+  }
+}
+game(playRound);*/  
 
 function game() {
-  for (let round = 0; round <= 5; round++) {
-  console.log(round);
-  playRound(userChoice, computerChoice);
+  for (let round = 0; round < 5; round ++) {
+    console.log(round);
+    let userChoice = (prompt ("Enter Rock, Paper, or Scissors"));
+    console.log("user", userChoice);
+    let computerChoice = getComputerChoice();
+    console.log("computer", computerChoice);
+    playRound(userChoice, computerChoice);
   }
-  }
-game();
+    console.log("userScore", userScore);
+    console.log("computerScore", computerScore);
+    if (userScore > computerScore) {
+      return "You Win!";
+    } else if (userScore < computerScore) {
+      return "You Lose!";
+    } else {
+      return "You Tie!";
+    }
+}
+console.log(game());
